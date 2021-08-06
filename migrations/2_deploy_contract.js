9b64b6d2d47e354e498b595cafc09cdcbb8bb66f
yeosiowvic <victoryeo1974@gmail.com>
2021-08-06 17:33:01 +0800

    deploy BConst contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,11 +1,13 @@
 const TMath = artifacts.require('TMath');
 const BToken = artifacts.require('BToken');
 const BFactory = artifacts.require('BFactory');
+const BConst = artifacts.require('BConst');
 
 module.exports = async function (deployer, network, accounts) {
     if (network === 'development' || network === 'coverage') {
         deployer.deploy(TMath);
     }
+    deployer.deploy(BConst);
     deployer.deploy(BFactory);
 };
 
