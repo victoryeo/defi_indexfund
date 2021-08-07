ddd2f8c2bd8ecb7447440d8e8e69f9f6d510a4c9
yeosiowvic <victoryeo1974@gmail.com>
2021-08-07 10:49:42 +0800

    add pool test cases

---
+++ test/pool.test.js
@@ -0,0 +1,71 @@
+const BPool = artifacts.require('BPool');
+const BFactory = artifacts.require('BFactory');
+const TToken = artifacts.require('TToken');
+const verbose = process.env.VERBOSE;
+
+contract('BPool', async (accounts) => {
+    const admin = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const { toWei } = web3.utils;
+    const { fromWei } = web3.utils;
+    const errorDelta = 10 ** -8;
+    const MAX = web3.utils.toTwosComplement(-1);
+
+    let WETH; let MKR; let DAI; let XXX; // addresses
+    let weth; let mkr; let dai; let xxx; // TTokens
+    let factory; // BPool factory
+    let pool; // first pool w/ defaults
+    let POOL; //   pool address
+
+    before(async () => {
+        factory = await BFactory.deployed();
+
+        POOL = await factory.newBPool.call();
+        await factory.newBPool();
+        pool = await BPool.at(POOL);
+
+        weth = await TToken.new('Wrapped Ether', 'WETH', 18);
+        mkr = await TToken.new('Maker', 'MKR', 18);
+        dai = await TToken.new('Dai Stablecoin', 'DAI', 18);
+        xxx = await TToken.new('XXX', 'XXX', 18);
+
+        WETH = weth.address;
+        MKR = mkr.address;
+        DAI = dai.address;
+        XXX = xxx.address;
+
+        /*
+            Tests assume token prices
+            WETH - $200
+            MKR  - $500
+            DAI  - $1
+            XXX  - $0
+        */
+
+        // Admin balances
+        await weth.mint(admin, toWei('50'));
+        await mkr.mint(admin, toWei('20'));
+        await dai.mint(admin, toWei('10000'));
+        await xxx.mint(admin, toWei('10'));
+
+        // User1 balances
+        await weth.mint(user1, toWei('25'), { from: admin });
+        await mkr.mint(user1, toWei('4'), { from: admin });
+        await dai.mint(user1, toWei('40000'), { from: admin });
+        await xxx.mint(user1, toWei('10'), { from: admin });
+
+        // User2 balances
+        await weth.mint(user2, toWei('12.2222'), { from: admin });
+        await mkr.mint(user2, toWei('1.015333'), { from: admin });
+        await dai.mint(user2, toWei('0'), { from: admin });
+        await xxx.mint(user2, toWei('51'), { from: admin });
+    });
+
+    describe('Binding Tokens', () => {
+        it('Controller is msg.sender', async () => {
+            const controller = await pool.getController();
+            assert.equal(controller, admin);
+        });
+    })
+})
