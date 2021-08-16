2d7d12b9fe939498af44104f826d6c96f9115cc2
yeosiowvic <victoryeo1974@gmail.com>
2021-08-16 09:41:28 +0800

    deploy oracle contract using truffle

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,6 +2,7 @@ const TMath = artifacts.require('TMath');
 const BToken = artifacts.require('BToken');
 const BFactory = artifacts.require('BFactory');
 const BConst = artifacts.require('BConst');
+const POracle = artifacts.require('POracle');
 
 module.exports = async function (deployer, network, accounts) {
     if (network === 'development' || network === 'coverage') {
@@ -9,5 +10,6 @@ module.exports = async function (deployer, network, accounts) {
     }
     deployer.deploy(BConst);
     deployer.deploy(BFactory);
+    deployer.deploy(POracle);
 };
 
