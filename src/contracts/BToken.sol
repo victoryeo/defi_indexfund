dd8e5f0dd11c1e7b430ba351656244e963528b1f
yeosiowvic <victoryeo1974@gmail.com>
2022-11-20 09:31:57 +0800

    fix shadowing and missing override

--- src/contracts/BToken.sol
+++ src/contracts/BToken.sol
@@ -40,27 +40,27 @@ contract BTokenBase is BNum {
     mapping(address => mapping(address=>uint)) internal _allowance;
     uint internal _totalSupply;
 
-    event Approval(address indexed src, address indexed dst, uint amt);
-    event Transfer(address indexed src, address indexed dst, uint amt);
+    event B_Approval(address indexed src, address indexed dst, uint amt);
+    event B_Transfer(address indexed src, address indexed dst, uint amt);
 
     function _mint(uint amt) internal {
         _balance[address(this)] = badd(_balance[address(this)], amt);
         _totalSupply = badd(_totalSupply, amt);
-        emit Transfer(address(0), address(this), amt);
+        emit B_Transfer(address(0), address(this), amt);
     }
 
     function _burn(uint amt) internal {
         require(_balance[address(this)] >= amt, "ERR_INSUFFICIENT_BAL");
         _balance[address(this)] = bsub(_balance[address(this)], amt);
         _totalSupply = bsub(_totalSupply, amt);
-        emit Transfer(address(this), address(0), amt);
+        emit B_Transfer(address(this), address(0), amt);
     }
 
     function _move(address src, address dst, uint amt) internal {
         require(_balance[src] >= amt, "ERR_INSUFFICIENT_BAL");
         _balance[src] = bsub(_balance[src], amt);
         _balance[dst] = badd(_balance[dst], amt);
-        emit Transfer(src, dst, amt);
+        emit B_Transfer(src, dst, amt);
     }
 
     function _push(address to, uint amt) internal {
@@ -90,19 +90,19 @@ contract BToken is BTokenBase, IERC20 {
         return _decimals;
     }
 
-    function allowance(address src, address dst) external view returns (uint) {
+    function allowance(address src, address dst) external view override returns (uint) {
         return _allowance[src][dst];
     }
 
-    function balanceOf(address whom) external view returns (uint) {
+    function balanceOf(address whom) external view override returns (uint) {
         return _balance[whom];
     }
 
-    function totalSupply() public view returns (uint) {
+    function totalSupply() public view override returns (uint) {
         return _totalSupply;
     }
 
-    function approve(address dst, uint amt) external returns (bool) {
+    function approve(address dst, uint amt) external override returns (bool) {
         _allowance[msg.sender][dst] = amt;
         emit Approval(msg.sender, dst, amt);
         return true;
@@ -125,12 +125,12 @@ contract BToken is BTokenBase, IERC20 {
         return true;
     }
 
-    function transfer(address dst, uint amt) external returns (bool) {
+    function transfer(address dst, uint amt) external override returns (bool) {
         _move(msg.sender, dst, amt);
         return true;
     }
 
-    function transferFrom(address src, address dst, uint amt) external returns (bool) {
+    function transferFrom(address src, address dst, uint amt) external override returns (bool) {
         require(msg.sender == src || amt <= _allowance[src][msg.sender], "ERR_BTOKEN_CALLER_NOT_ALLOWED");
         _move(src, dst, amt);
         if (msg.sender != src && _allowance[src][msg.sender] != uint256(-1)) {
